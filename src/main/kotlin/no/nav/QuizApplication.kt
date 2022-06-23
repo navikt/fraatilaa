a7e04badba588cf2c6acdb4237072601f39cdc8e
Åshild Tenold Fridtun <ashildtenoldfridtun@Ashilds-MacBook-Pro.local>
2022-06-23 14:01:38 +0200

    first task

--- src/main/kotlin/no/nav/QuizApplication.kt
+++ src/main/kotlin/no/nav/QuizApplication.kt
@@ -28,7 +28,7 @@ class QuizApplication(private val teamName: String, database: Database? = null):
      */
 
     private fun handleRegisterTeam(question: Question) {
-        TODO("Her må du skrive kode ;)")
+        answer("test", "123", "fraAtilAA")
     }
 
 }
